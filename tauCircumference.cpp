b349b013164cee72d6233cf0408427d0d6d07a1d
Ubuntu <ubuntu@ip-172-31-84-56.ec2.internal>
2022-05-22 19:09:55 +0000

    init

--- tauCircumference.cpp
+++ tauCircumference.cpp
@@ -8,20 +8,14 @@
 #include <iostream>
 
 int main() {
-    // I calculate circumference of a circle
-    const double TAU = 6.28;
-    int radius;
-    double circumference;
-
-    // input
-    std::cout << "Enter radius of the circle in mm: ";
-    std::cin >> radius;
-
-    // process
-    circumference = TAU*radius;
-
+    // I display the values of rgb
     // output
-    std::cout << "" << std::endl;
-    std::cout << "Circumference is " << circumference << " mm" << std::endl;
-    std::cout << "\nDone." << std::endl;
+    for (int counter1 = 0; counter1 <= 256; counter1++) {
+        for (int counter2 = 0; counter2 <= 256; counter2++) {
+            for (int counter3 = 0; counter3 <= 256; counter3++) {
+                std::cout << "RGB(" << counter1 << ", " << counter2 << ", ";
+                std::cout << counter3 << ")" << std::endl;
+            }
+        }
+    }
 }
